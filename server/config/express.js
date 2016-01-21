1793d0c51c66c087724631e7034d81accbfa36a6
samutamm <samutamm@gmail.com>
2016-01-21 14:30:41 +0100

    Enable cors

--- server/config/express.js
+++ server/config/express.js
@@ -16,6 +16,12 @@ module.exports = function() {
   }));
   app.use(bodyParser.json());
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   require('../app/routes/apartements.route.js')(app);
 
   app.use(express.static('public'));
