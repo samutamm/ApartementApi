b58057fcf9bf1cf42791c6e1817f17a5b017fbe0
samutamm <samutamm@gmail.com>
2016-02-15 21:08:05 +0100

    Test apartement model.

--- test/ApartementTest.js
+++ test/ApartementTest.js
@@ -1,11 +1,19 @@
-import Apartement from './../server/app/models/Apartement.js';
-import {expect} from 'chai';
+const Apartement = require('./../server/app/models/Apartement.js');
+const expect = require('chai').expect;
 
-describe('Apartement', () => {
+describe('Apartement', function () {
 
-  it('is valid with valid params', () => {
-    let apartement = new Apartement('name', 'address', 'City', 'agent', 123);
-    console.log(apartement.asAList());
+  const json = { 'name': 'N', 'address': 'a', 'city': 'bogoss', 'price': 43123};
+
+  it('is valid with valid params', function () {
+    var apartement = new Apartement(json);
     expect(apartement.isValid()).to.equal(true);
+  });
+
+  it('is not valid with unvalid params', function () {
+    var copy = JSON.parse(JSON.stringify(json));
+    copy.name = undefined;
+    var apartement = new Apartement(copy);
+    expect(apartement.isValid()).to.equal(false);
   });
 });
